feba2ca4d326642813335ac3094387701d231ff0
JO <jo@JOui-MacBookAir.local>
2020-12-31 20:58:22 +0900

    https://github.com/mnj190/react_test.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    )
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul class="li"><a href="1.html">HTML</a></ul>
+        <ul class="li"><a href="2.html">CSS</a></ul>
+        <ul class="li"><a href="3.html">JavaScript </a></ul>
+      </nav>
+    )
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          WEB
+        </h1>
+        world wild web
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
