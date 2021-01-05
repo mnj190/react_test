48641fa8fbc6fd2d49373a93c6e65ba2d94e72b7
JO <mnj190@naver.com>
2021-01-05 22:15:26 +0900

    react_19.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import './App.css';
 import TOC from "./components/TOC"
-import Content from "./components/Content"
+import ReadContent from "./components/ReadContent"
+import Control from "./components/Control"
 import Subject from "./components/Subject"
+import CreateContent from "./components/CreateContent";
 
 class App extends Component {
   constructor(props) {
@@ -21,10 +23,11 @@ class App extends Component {
   }
   render() {
     console.log("App render")
-    var _title, _desc = null;
+    var _title, _desc, _acticle = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _acticle = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -36,7 +39,9 @@ class App extends Component {
         }
         i++;
       }
-
+      _acticle = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if(this.state.mode === "create") {
+      _acticle = <CreateContent></CreateContent>
     }
     return (
       <div className="App">
@@ -54,7 +59,14 @@ class App extends Component {
             selected_content_id: Number(id)
           })
         }.bind(this)} data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function (_mode) {
+          this.setState({
+            mode: _mode
+          })
+        }.bind(this)}></Control>
+
+        {_acticle}
+
       </div>
     );
   }
---
+++ src/components/CreateContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CreateContent extends Component {
+  render() {
+    return (
+      <article>
+        <h2>Create</h2>
+        <from></from>
+      </article>
+    )
+  }
+}
+
+export default CreateContent;
